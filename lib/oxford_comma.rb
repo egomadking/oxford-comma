1955a6077fe6d00777c99a0c3044a581234add2d
Roy Mosby <ego.madking@gmail.com>
2020-12-09 09:53:02 -0500

    commit tree 8df47d0d23f48c9f967391fe52af156a38f2c68f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.length() < 2
+    array.join
+  elsif array.length() == 2
+    array.join(" and ")
+  else
+    oxford_formatted = ""
+    array.each do |element|
+      if array.index(element) == 0
+        oxford_formatted << element        
+      elsif array.index(element) > 0 && array.index(element) < (array.length() - 1)
+        oxford_formatted << ", #{element}"
+      else
+        oxford_formatted << ", and #{element}"
+      end
+    end
+    return oxford_formatted
+  end
 end
